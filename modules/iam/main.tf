bdc1b679d5b6153421b7921eb163b17ff5d96f8f
Jay Simoni <jsimoni@users.noreply.github.com>
2021-12-06 20:19:01 -0500

    Update main.tf

---
+++ modules/iam/main.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_policy" "policy" {
+  name        = "sample-policy"
+  description = "My sample policy"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.bucket.arn}"
+    }
+  ]
+
+}
+EOT
+}
