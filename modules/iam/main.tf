1109731d17c5f39c7c99347aa0aed65282442ac4
Oludare Odo <darekorex143@gmail.com>
2022-01-18 09:10:03 -0600

    test

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,45 +1,45 @@
-#resource "aws_iam_policy" "policy" {
- # name        = "my-sample-policy"
- # description = "My sample policy"
+resource "aws_iam_policy" "policy" {
+  name        = "my-sample-policy"
+  description = "My sample policy"
 
-  #policy = <<EOT
-#{
- # "Version": "2012-10-17",
-  #"Statement": [
-   # {
-    #  "Action": [
-     #   "s3:ListAllMyBuckets"
-      #],
-      #"Effect": "Allow",
-      #"Resource": "*"
-    #}
-  #]
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
 
-#}
-#EOT
-#}
+}
+EOT
+}
 
 
-#resource "aws_iam_policy" "policy2" {
- # name        = "another-sample-policy"
-  #description = "Another sample policy"
+resource "aws_iam_policy" "policy2" {
+  name        = "another-sample-policy"
+  description = "Another sample policy"
 
-  #policy = <<EOT
-#{
- # "Version": "2012-10-17",
-  #"Statement": [
-   # {
-    #  "Action": [
-     #   "*"
-      #],
-      #"Effect": "Deny",
-      #"Resource": "*"
-    #}
-  #]
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "*"
+      ],
+      "Effect": "Deny",
+      "Resource": "*"
+    }
+  ]
 
-#}
-#EOT
-#}
+}
+EOT
+}
 
 resource "aws_vpc" "test-vpc" {
   cidr_block       = "10.0.0.0/16"
